7a613c0dca7cddb9b080a8b0a9261dcd392fe978
alandev23 <azulceba@hotmail.com>
2017-08-27 21:01:23 -0500

    AVANCE 1
    CUERPO DE LA PAGINA

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+<script>
+  $("#zoom_05").elevateZoom({ zoomType    : "inner", cursor: "crosshair" });
+</script>
+
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+    $(".mCustomScrollbar").mCustomScrollbar({axis:"x"});
+});
+</script>
